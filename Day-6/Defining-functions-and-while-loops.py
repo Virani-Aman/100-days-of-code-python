e0a7f20dc268ab5ba4f987c436794d86e4216064
Aman Virani <viranipaman@gmail.com>
2023-11-15 09:47:33 +0530

    Day-6  completed

---
+++ Day-6/Defining-functions-and-while-loops.py
@@ -0,0 +1,18 @@
+# So lets say you have created a program or something and you need to perform the same multiple task multiple times
+# And because of which your code looks like junk to make your work easier and your code more readable we define functions like this 
+
+def my_function():
+    print("Dance")
+    print("bale bale")
+    print("I am batman")
+
+my_function()
+
+# while loops while loops are just like for loops but they perform a task while its true or false like
+
+number = 10
+
+while number != 0:
+    my_function()
+    number -= 1
+    print(number)
